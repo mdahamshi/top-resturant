93a9af4f587adf864294b97e032efe73e7bcefcd
Mohammad <mohammad.dahamshi@gmail.com>
2025-06-17 22:27:34 +0300

    Implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./styles.css";
-
+import logoImage from "../asset/food.svg";
+import heroimg from "../asset/bg.jpg"
 import { greeting } from "./greeting.js";
+import {View} from './view.js';
+
+new View();
+const logo = document.querySelector('#nav-logo');
+logo.src = logoImage;
+
+const div = document.querySelector('.hero-section');
+div.style.backgroundImage = `radial-gradient(ellipse at center, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${heroimg})`;
+
 
 console.log(greeting);
---
+++ src/view.js
@@ -0,0 +1,39 @@
+import  home from   './pages/home.js';
+import menu from './pages/menu.js';
+import about from './pages/about.js';
+
+class View {
+
+    constructor(){
+        this.content = document.getElementById('content');
+        this.initPages();
+
+        this.replaceContent(this.pages.home);
+        this.addEvents();
+    }
+    initPages(){
+        this.pages.home = home();
+        this.pages.menu = menu();
+        this.pages.about = about();
+    }
+    pages = {};
+    replaceContent(newContent){
+        let c = this.content;
+        let child = c.firstChild;
+        c.replaceChild(newContent, child);
+    }
+    actions = ['home', 'about', 'menu'];
+    addEvents(){
+        const buttons = document.querySelectorAll('.flex-row button');
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const action = button.dataset.action;
+                if(this.actions.includes(action))
+                    this.replaceContent(this.pages[action])
+            });
+        });
+    }
+}
+
+
+export {View};
